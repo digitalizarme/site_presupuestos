8fcc9f4fbb906778fc4b0c8264b1b7154e942bec
Rodrigo Lucas Pereira <rodrigoyleila@gmail.com>
2019-03-09 21:55:37 -0300

    agora vai pra producao

--- src/common/constantesGenerales.js
+++ src/common/constantesGenerales.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV === 'development') {
   api_string = 'http://localhost:3000';
 }
 else{
-  api_string = 'https://apidigitalizarme.herokuapp.com/';
+  api_string = 'https://apidigitalizarme.herokuapp.com';
 }
 
 export const API = api_string;
